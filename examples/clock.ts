c3e913b75b273eea5fe90a6e31fe42645bc2fc4a
Milos Stanojevic <37949266+pointermess@users.noreply.github.com>
2020-02-23 23:06:18 +0100

    Create clock.ts

---
+++ examples/clock.ts
@@ -0,0 +1,33 @@
+/* Supercharge.js Clock Example
+ *
+ * This example shows a simple clock displaying the time and date.
+ * It uses bindings to display the time and an interval to update the values.
+ */
+class Clock extends SuperchargeBindable
+{
+    constructor() {
+        super('div', 'The current time is {time} and the date is {date}.');
+
+        // binds the variables and initialize with default values
+        this.bind('time', this.getDate().time);
+        this.bind('date', this.getDate().date);
+
+        // set the time and date every second
+        setInterval(() => {
+            this.setBinding('time', this.getDate().time);
+            this.setBinding('date', this.getDate().date);
+        }, 1000);
+    }
+
+    getDate() {
+        var date = new Date();
+        return {
+            time: date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds(),
+            date: date.getDate() + '. ' + date.getMonth() + '. ' + date.getFullYear()
+        }
+    }
+}
+
+window.onload = function(){
+    new Clock().mount(document.body);
+};
