076fdbfa217882262941fb53c5aaf1ca590ce7a8
Milos Stanojevic <37949266+pointermess@users.noreply.github.com>
2020-03-05 17:24:29 +0100

    "owned" functions, basic viewer error handling & refactorings

--- src/Supercharge.js
+++ src/Supercharge.js
@@ -1,8 +1,15 @@
-var Supercharge = (function () {
+/**
+ * Supercharge Class
+ *
+ * Basic Supercharge element.
+ */
+var Supercharge = /** @class */ (function () {
     function Supercharge(tag, body) {
         var _this = this;
         if (body === void 0) { body = undefined; }
+        // create element
         var node;
+        // append body
         this.element = document.createElement(tag);
         if (typeof body == "string") {
             node = document.createTextNode(body);
@@ -11,51 +18,29 @@ var Supercharge = (function () {
         else if (typeof body == "object") {
             if (body instanceof Supercharge)
                 this.element.appendChild(body.element);
-            else if (body instanceof Node)
+            else if (body instanceof HTMLElement)
                 this.element.appendChild(body);
-            else {
+            else if (Array.isArray(body)) {
                 for (var bodyIndex in body) {
                     if (body.hasOwnProperty(bodyIndex))
                         body[bodyIndex].mount(this.element);
                 }
             }
+            else
+                console.error('Unknown body type');
         }
+        // bind events
         this.element.onclick = function (e) { return _this.onClick(e); };
         this.element.onload = function () { return _this.onLoad(); };
+        this.element.onmousemove = function (e) { return _this.onMouseMove(e); };
         this.onCreate();
     }
-    Supercharge.prototype.setId = function (id) {
-        this.element.id = id;
-    };
-    Supercharge.prototype.setAttribute = function (attr, value) {
-    };
-    Supercharge.prototype.addClass = function (attr, value) {
-    };
-    Supercharge.prototype.removeClass = function (attr, value) {
-    };
-    Supercharge.prototype.hasClass = function (attr, value) {
-    };
-    Supercharge.prototype.toggleClass = function (attr, value) {
-    };
-    Supercharge.prototype.mount = function (element) {
-        this.onMount();
-        if (element instanceof Node)
-            element.appendChild(this.element);
-        else if (element instanceof Supercharge)
-            element.element.appendChild(this.element);
-    };
-    Supercharge.prototype.unmount = function () {
-        this.element.parentNode.removeChild(this.element);
-    };
-    Supercharge.prototype.insert = function (array) {
-        for (var item in array) {
-            if (array.hasOwnProperty(item)) {
-                array[item].mount(this);
-                console.log(array[item]);
-            }
-        }
-    };
+    /*
+     * Events
+     */
     Supercharge.prototype.onClick = function (e) { };
+    ;
+    Supercharge.prototype.onMouseMove = function (e) { };
     ;
     Supercharge.prototype.onLoad = function () { };
     ;
@@ -63,55 +48,189 @@ var Supercharge = (function () {
     ;
     Supercharge.prototype.onCreate = function () { };
     ;
+    Supercharge.prototype.setId = function (id) {
+        this.element.id = id;
+    };
+    Supercharge.prototype.setAttribute = function (attr, value) {
+        this.element.setAttribute(attr, value);
+    };
+    Supercharge.prototype.getAttribute = function (attr) {
+        return this.element.getAttribute(attr);
+    };
+    Supercharge.prototype.addClass = function (className) {
+        if (!this.hasClass(className))
+            this.element.className += " " + className;
+    };
+    Supercharge.prototype.removeClass = function (className) {
+        if (this.element.classList)
+            this.element.classList.remove(className);
+        else
+            this.element.className = this.element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    };
+    Supercharge.prototype.hasClass = function (className) {
+        return !!this.element.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+    };
+    Supercharge.prototype.toggleClass = function (className) {
+        if (this.hasClass(className))
+            this.removeClass(className);
+        else
+            this.addClass(className);
+    };
+    Supercharge.prototype.setStyle = function (property, value) {
+        this.element.style[property] = value;
+    };
+    Supercharge.prototype.mount = function (element) {
+        this.onMount();
+        if (element instanceof HTMLElement)
+            element.appendChild(this.element);
+        else if (element instanceof Supercharge)
+            element.element.appendChild(this.element);
+    };
+    Supercharge.prototype.unmount = function () {
+        if (this.element.parentNode !== null)
+            this.element.parentNode.removeChild(this.element);
+    };
+    Supercharge.prototype.insert = function (array) {
+        if (!Array.isArray(array)) {
+            console.error('Only arrays can be passed to insert.');
+            return;
+        }
+        for (var item in array) {
+            if (array.hasOwnProperty(item)) {
+                if (array[item] instanceof Supercharge)
+                    array[item].mount(this);
+                else if (array[item] instanceof HTMLElement)
+                    array[item].element.appendChild(array[item]);
+                else
+                    console.error('Unknown element type in Supercharge.insert.');
+            }
+        }
+    };
     return Supercharge;
 }());
-var SuperchargeFactory = (function () {
+/**
+ * SuperchargeViewer Class
+ *
+ * Container class to set
+ */
+var SuperchargeViewer = /** @class */ (function () {
+    function SuperchargeViewer(parent) {
+        this.currentView = undefined;
+        this.parent = parent;
+    }
+    SuperchargeViewer.prototype.setView = function (view) {
+        var _this = this;
+        this.onChangeView(function () {
+            if (_this.currentView != undefined) {
+                if (_this.currentView instanceof Supercharge)
+                    _this.currentView.unmount();
+                else if (Array.isArray(_this.currentView)) {
+                    for (var index in _this.currentView) {
+                        if (_this.currentView.hasOwnProperty(index))
+                            _this.currentView[index].unmount();
+                    }
+                }
+            }
+            _this.currentView = view;
+            if (view instanceof Supercharge)
+                view.mount(_this.parent);
+            else
+                _this.parent.insert(view);
+            _this.onViewChanged();
+        });
+    };
+    SuperchargeViewer.prototype.removeView = function () {
+        if (this.currentView instanceof Supercharge) {
+            this.currentView.unmount();
+            this.currentView = undefined;
+        }
+    };
+    // Events
+    SuperchargeViewer.prototype.onChangeView = function (continueFn) { continueFn(); };
+    SuperchargeViewer.prototype.onViewChanged = function () { };
+    return SuperchargeViewer;
+}());
+/**
+ * factory class to build trees of Supercharge objects
+ */
+var SuperchargeFactory = /** @class */ (function () {
     function SuperchargeFactory() {
     }
+    /**
+     * Builds a tree of Supercharge objects
+     * @param input object
+     * @return Built instance of a Supercharge object
+     */
     SuperchargeFactory.build = function (input, parent) {
         if (parent === void 0) { parent = undefined; }
         var node;
+        // experimental feature
+        var parentNode = node;
+        if (parent != undefined)
+            parentNode = parent;
         if (input instanceof Supercharge) {
             return input;
         }
         else {
             node = new Supercharge(input.tag);
+            // TODO: finish build process
             if (typeof input.body == "string") {
                 node.element.appendChild(document.createTextNode(input.body));
             }
             else if (typeof input.body == "object") {
                 for (var inputNode in input.body) {
                     if (input.body.hasOwnProperty(inputNode)) {
-                        var childNode = this.build(input.body[inputNode], node);
+                        var childNode = this.build(input.body[inputNode], parentNode);
                         node.element.appendChild(childNode.element);
                     }
                 }
             }
         }
-        var parentNode = node;
-        if (parent != undefined)
-            parentNode = parent;
+        // id, classes and styles
+        if (typeof input.id == "string")
+            node.setId(input.id);
+        if (typeof input.classes == "object") {
+            for (var className in input.classes) {
+                if (input.classes.hasOwnProperty(className))
+                    node.addClass(input.classes[className]);
+            }
+        }
+        if (typeof input.attributes == "object") {
+            for (var attributeName in input.attributes) {
+                if (input.attributes.hasOwnProperty(attributeName))
+                    node.setAttribute(attributeName, input.attributes[attributeName]);
+            }
+        }
         if (typeof input.onClick == "function")
             node.onClick = input.onClick.bind(parentNode);
         if (typeof input.onMount == "function")
             node.onMount = input.onMount.bind(parentNode);
         if (typeof input.onCreate == "function")
             node.onMount = input.onCreate.bind(node);
+        // functions
         for (var inputFunction in input.functions) {
             if (input.functions.hasOwnProperty(inputFunction)) {
                 parentNode[inputFunction] = input.functions[inputFunction];
                 console.log('added to root: ' + inputFunction);
             }
         }
+        // owned functions
+        // bound to the created node instead of the parent node
+        for (var inputFunction in input.owned) {
+            if (input.owned.hasOwnProperty(inputFunction)) {
+                parentNode[inputFunction] = input.owned[inputFunction].bind(node);
+            }
+        }
         return node;
     };
-    SuperchargeFactory.buildArray = function (input) {
+    SuperchargeFactory.buildArray = function (input, parentNode) {
+        if (parentNode === void 0) { parentNode = undefined; }
         var result = [];
         for (var inputNode in input) {
-            result.push(this.build(input[inputNode]));
+            result.push(this.build(input[inputNode], parentNode));
         }
         return result;
     };
     return SuperchargeFactory;
 }());
-//# sourceMappingURL=supercharge.js.map
+//# sourceMappingURL=Supercharge.js.map
--- src/Supercharge.ts
+++ src/Supercharge.ts
@@ -1,11 +1,10 @@
 /**
- * Supercharge class
+ * Supercharge Class
  *
- * Can be used as base to derive classes from or can be used to directly
- * work on Supercharge instances
+ * Basic Supercharge element.
  */
 class Supercharge {
-    public element;
+    public element : HTMLElement;
 
     constructor(tag: string, body: any = undefined) {
         // create element
@@ -14,92 +13,163 @@ class Supercharge {
         // append body
         this.element = document.createElement(tag);
 
-
-        if (typeof body == "string")
-        {
+        if (typeof body == "string") {
             node = document.createTextNode(body);
             this.element.appendChild(node);
         }
-        else if (typeof body == "object")
-        {
+        else if (typeof body == "object") {
             if (body instanceof Supercharge)
                 this.element.appendChild(body.element);
-            else if (body instanceof Node)
+            else if (body instanceof HTMLElement)
                 this.element.appendChild(body);
-            else
-            {
-                for (let bodyIndex in body)
-                {
+            else if (Array.isArray(body)) {
+                for (let bodyIndex in body) {
                     if (body.hasOwnProperty(bodyIndex))
                         body[bodyIndex].mount(this.element);
                 }
             }
+            else
+                console.error('Unknown body type');
         }
 
+        // bind events
         this.element.onclick = (e) => this.onClick(e);
         this.element.onload = () => this.onLoad();
+        this.element.onmousemove = (e) => this.onMouseMove(e);
 
         this.onCreate();
     }
 
-    public setId(id) {
+    /*
+     * Events
+     */
+    public onClick(e : MouseEvent) {};
+    public onMouseMove(e : MouseEvent) {};
+    public onLoad() {};
+    public onMount() {};
+    public onCreate() {};
+
+    public setId(id : string) : void {
         this.element.id = id;
     }
 
-    public setAttribute(attr, value) {
+    public setAttribute(attr : string, value : string) : void {
+        this.element.setAttribute(attr, value);
+    }
+
+    public getAttribute(attr : string) : string {
+        return this.element.getAttribute(attr);
+    }
+
+    public addClass(className : string) : void {
+        if (!this.hasClass(className))
+            this.element.className += " " + className;
+    }
+
+    public removeClass(className : string) : void {
+        if (this.element.classList)
+            this.element.classList.remove(className);
+        else
+            this.element.className = this.element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
 
     }
 
-    public addClass(attr, value) {
-
+    public hasClass(className : string) : boolean {
+        return !!this.element.className.match(new RegExp('(\\s|^)'+className+'(\\s|$)'));
     }
 
-    public removeClass(attr, value) {
-
+    public toggleClass(className : string) : void {
+        if (this.hasClass(className))
+            this.removeClass(className);
+        else
+            this.addClass(className);
     }
 
-    public hasClass(attr, value) {
-
+    public setStyle(property : string, value : string) : void {
+        this.element.style[property] = value;
     }
 
-    public toggleClass(attr, value) {
-
-    }
-
-    public mount(element) {
+    public mount(element : Supercharge|HTMLElement) : void {
         this.onMount();
-        if (element instanceof Node)
+
+        if (element instanceof HTMLElement)
             element.appendChild(this.element);
         else if (element instanceof Supercharge)
             element.element.appendChild(this.element);
     }
 
-    public unmount() {
-        this.element.parentNode.removeChild(this.element);
+    public unmount() : void {
+        if (this.element.parentNode !== null)
+            this.element.parentNode.removeChild(this.element);
     }
 
-    public insert(array)
-    {
-        for (let item in array)
-        {
-            if (array.hasOwnProperty(item))
-            {
-                array[item].mount(this);
-                console.log(array[item]);
+    public insert(array) : void {
+        if (!Array.isArray(array)) {
+            console.error('Only arrays can be passed to insert.');
+            return;
+        }
+
+        for (let item in array) {
+            if (array.hasOwnProperty(item)) {
+                if (array[item] instanceof Supercharge)
+                    array[item].mount(this);
+                else if (array[item] instanceof HTMLElement)
+                    array[item].element.appendChild(array[item]);
+                else
+                    console.error('Unknown element type in Supercharge.insert.');
             }
         }
     }
-
-
-    /*
-     * Events
-     */
-    public onClick(e) {};
-    public onLoad() {};
-    public onMount() {};
-    public onCreate() {};
 }
 
+/**
+ * SuperchargeViewer Class
+ *
+ * Container class to set
+ */
+class SuperchargeViewer {
+    private parent: Supercharge;
+    private currentView: Supercharge[]|Supercharge = undefined;
+
+    constructor(parent) {
+        this.parent = parent;
+    }
+
+    public setView(view: Supercharge|Array<Supercharge>) : void {
+        this.onChangeView(() => {
+            if (this.currentView != undefined) {
+                if (this.currentView instanceof Supercharge)
+                    this.currentView.unmount();
+                else if (Array.isArray(this.currentView)) {
+                    for (let index in this.currentView) {
+                        if (this.currentView.hasOwnProperty(index))
+                            this.currentView[index].unmount();
+                    }
+                }
+            }
+
+            this.currentView = view;
+
+            if (view instanceof Supercharge)
+                view.mount(this.parent);
+            else
+                this.parent.insert(view);
+
+            this.onViewChanged();
+        })
+    }
+
+    public removeView() : void {
+        if (this.currentView instanceof Supercharge) {
+            this.currentView.unmount();
+            this.currentView = undefined;
+        }
+    }
+
+    // Events
+    public onChangeView(continueFn) { continueFn() }
+    public onViewChanged() {}
+}
 
 /**
  * factory class to build trees of Supercharge objects
@@ -111,13 +181,16 @@ class SuperchargeFactory
      * @param input object
      * @return Built instance of a Supercharge object
      */
-    public static build(input, parent = undefined)
+    public static build(input, parent = undefined) : Supercharge
     {
         let node;
 
 
-        if (input instanceof Supercharge)
-        {
+        // experimental feature
+        let parentNode = node;
+        if (parent != undefined) parentNode = parent;
+
+        if (input instanceof Supercharge) {
             return input;
         }
         else
@@ -125,25 +198,37 @@ class SuperchargeFactory
             node = new Supercharge(input.tag);
 
             // TODO: finish build process
-            if (typeof input.body == "string")
-            {
+            if (typeof input.body == "string") {
                 node.element.appendChild(document.createTextNode(input.body));
             }
-            else if (typeof input.body == "object")
-            {
+            else if (typeof input.body == "object") {
                 for (let inputNode in input.body) {
-                    if (input.body.hasOwnProperty(inputNode))
-                    {
-                        let childNode = this.build(input.body[inputNode], node);
+                    if (input.body.hasOwnProperty(inputNode)) {
+                        let childNode = this.build(input.body[inputNode], parentNode);
                         node.element.appendChild(childNode.element);
                     }
                 }
             }
         }
 
-        // experimental feature
-        let parentNode = node;
-        if (parent != undefined) parentNode = parent;
+
+        // id, classes and styles
+        if (typeof input.id == "string")
+            node.setId(input.id);
+
+        if (typeof input.classes == "object") {
+            for (let className in input.classes) {
+                if (input.classes.hasOwnProperty(className))
+                    node.addClass(input.classes[className]);
+            }
+        }
+
+        if (typeof input.attributes == "object") {
+            for (let attributeName in input.attributes) {
+                if (input.attributes.hasOwnProperty(attributeName))
+                    node.setAttribute(attributeName, input.attributes[attributeName]);
+            }
+        }
 
         if (typeof input.onClick == "function") node.onClick = input.onClick.bind(parentNode);
         if (typeof input.onMount == "function") node.onMount = input.onMount.bind(parentNode);
@@ -151,10 +236,17 @@ class SuperchargeFactory
 
         // functions
         for (let inputFunction in input.functions) {
-            if (input.functions.hasOwnProperty(inputFunction))
-            {
+            if (input.functions.hasOwnProperty(inputFunction)) {
                 parentNode[inputFunction] = input.functions[inputFunction];
                 console.log('added to root: ' + inputFunction);
+            }
+        }
+
+        // owned functions
+        // bound to the created node instead of the parent node
+        for (let inputFunction in input.owned) {
+            if (input.owned.hasOwnProperty(inputFunction)) {
+                parentNode[inputFunction] = input.owned[inputFunction].bind(node);
             }
         }
 
@@ -162,13 +254,11 @@ class SuperchargeFactory
     }
 
 
-    public static buildArray(input) : any
-    {
-        let result = [];
-
+    public static buildArray(input : Array<any>, parentNode : string = undefined) : Supercharge[] {
+        let result : Supercharge[] = [];
 
         for (let inputNode in input) {
-            result.push(this.build(input[inputNode]));
+            result.push(this.build(input[inputNode], parentNode));
         }
 
         return result;
